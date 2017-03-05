fa0f68e412b4d19bc6ef6198291433fad95ed273
nelemersch <nele.mersch5@googlemail.com>
2017-03-05 21:56:39 +0100

    fix bug

--- PHP/userOverview.php
+++ PHP/userOverview.php
@@ -2,6 +2,7 @@
 	 ob_start();
 	 session_start();
 	 require_once '../HTML/userOverview.html';
+	 require_once("DBConnection.php");
 	 
 	 // if session is not set this will redirect to login page
 	 if( !isset($_SESSION['user']) ) {
@@ -9,6 +10,7 @@
 	  exit;
 	 }
 	 // select loggedin users detail
-	 $res=iiigel_query("SELECT * FROM users WHERE ID=".$_SESSION['user']);
-	 $userRow=mysql_fetch_array($res);
+	 $query = "SELECT * FROM users WHERE ID=".$_SESSION['user'];
+	 $res=iiigel_query($query);
+	 $userRow=mysqli_fetch_array($res);
 ?>

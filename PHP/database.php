9f4e0415425999b27eca0d2b24d5c43401ea09bc
jassiii55 <jasmina_karakas@hotmail.de>
2017-03-15 19:02:45 +0100

    preparedstmts

--- PHP/database.php
+++ PHP/database.php
@@ -26,7 +26,7 @@
                 throw new exception('Mehr als ein User mit dieser ID');        
             }
         }
-        public function getInstitutionFromId($ID){
+        public function getInstitutionFromID($ID){
             $res = $this->query("SELECT * FROM Institutions WHERE Institutions.ID ='$ID'");
             if (mysqli_num_rows($res)==1){
                 $row = mysqli_fetch_array($res);

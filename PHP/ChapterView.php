0469bb070157ef2b5e64e5a12e6dcb6fcace96d5
nelemersch <nele.mersch5@googlemail.com>
2017-03-22 21:38:37 +0100

    Added half functional buttons and a modal

--- PHP/ChapterView.php
+++ PHP/ChapterView.php
@@ -8,7 +8,13 @@
     $myModuleID = $_GET['moduleID'];
     $myChapterID = $_GET['chapterID'];
 
-	 
+    if ( isset($_POST['NextButton']) ) {
+    }
+
+     if ( isset($_POST['AbgabeButton']) ) {
+         
+    }
+
 	 // if session is not set this will redirect to login page
 	 if( !isset($_SESSION['user']) ) {
 	  header("Location: index.php");
@@ -18,16 +24,34 @@
     //
 
     $myModule = $ODB->getModuleFromID($myModuleID);
+   
     $search = array('%ChapterHeadline%','%ChapterText%');
     $replace = array($myModule->getChapterHeadlineByIndex($myChapterID),$ODB->replaceTags($myModule->getChapterTextbyIndex($myChapterID)));
     $myPage = str_replace($search,$replace,$myPage);
 
+   
+    $toAdd = "";
+
+    if($myModule->chapter[$myChapterID]->getbIsMandatoryHandIn()) {
+        $toAdd = file_get_contents('../HTML/ChapterViewButtonAbgabe.html');
+        
+    }else{
+        $toAdd = file_get_contents('../HTML/ChapterViewButtonNextChapter.html');  
+        $search = array('%Link%');
+        $iactIndex = $myModule->chapter[$myChapterID]->getiIndex()+1;
+        $replace = array("/iiigel/PHP/chapterView.php?moduleID=".$myModuleID."&chapterID=".$iactIndex );
+        $toAdd = str_replace($search,$replace,$toAdd); 
+    }
+
+    $search = array('%Buttons%');;
+    $replace = array($toAdd);
+    $myPage = str_replace($search,$replace,$myPage);
 
     $toAdd = ""; //Hinzugefügter HTML Code
    for ($i=0; $i< sizeof($myModule->chapter);$i++){  
             $myRow = file_get_contents('../HTML/ChapterViewListItem.html');
             $search = array('%ChapterTitle%','%Link%');
-            $replace = array($myModule ->chapter[$i]->getsTitle(),"/iiigel/PHP/chapterView.php?moduleID=".$myModuleID."&chapterID=".$i."" );
+            $replace = array($myModule ->chapter[$i]->getsTitle(),"/iiigel/PHP/chapterView.php?moduleID=".$myModuleID."&chapterID=".$i );
             $myRow = str_replace($search,$replace,$myRow);
         
         $toAdd = $toAdd . $myRow;

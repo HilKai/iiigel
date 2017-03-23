5d645aac6636dbe9f7371bf49d4c5b4e9a9e3743
HilKai <Kai.Hilgers.Info@gmx.de>
2017-03-23 18:56:29 +0100

    todo Complete this

--- PHP/editProfile.php
+++ PHP/editProfile.php
@@ -9,6 +9,8 @@
         header("Location: index.php");
         exit;
     }
+    $myUser = $ODB->getUserFromID($_SESSION['user']);
+
 	
 	if ( isset($_POST['btn-save']) ) {
         $error = false;
@@ -28,10 +30,7 @@
 		$email = trim($_POST['email']);
 		$email = strip_Tags($email);
 		$email = htmlspecialchars($email);
-		
-		$passwort = trim($_POST['passwort']);
-		$passwort = strip_Tags($passwort);
-		$passwort = htmlspecialchars($passwort);
+
 		
 		
 		if ((!empty($username))&&(strlen($username)<3)) {
@@ -67,11 +66,11 @@
 
 		
       }
-         
+ 
 ?>
 
-<!DOCTYPE html>
-<html>
+    <!DOCTYPE html>
+    <html>
 
     <head>
         <meta charset="utf-8">
@@ -94,7 +93,7 @@
                     <img id="ProfilePic" class="img-responsive img-circle" src="../ProfilePics/profilePic.jpg" alt="ProfilePicture">
 
                 </div>
-                
+
                 <div id="ProfileTextContainer" class="col-md-8">
 
                     <div id="" class="row">
@@ -102,69 +101,72 @@
                             <h1 id="Heading"> Profil bearbeiten</h1>
                         </div>
                     </div>
-                    
+
                     <div id="" class="row">
                         <div class="col-md-6 noPadding">
                             <form action="upload.php" method="post" enctype="multipart/form-data">
-                               <div class="form-group">
+                                <div class="form-group">
                                     <label for="exampleInputFile"><span class="glyphicon glyphicon-picture" aria-hidden="true"></span> Profilbild hochladen</label>
                                     <input type="file" name="datei" value="Hochladen" id="exampleInputFile">
-                               </div> 
+                                </div>
                             </form>
                         </div>
-                        
+
                         <div class="col-md-2 noPadding">
                             <button id="PasswortButton" class="btn btn-default"><span class="glyphicon glyphicon-pencil"></span> Passwort ändern</button>
                         </div>
                     </div>
-                    
-                    <div class="row">
-                        <div class="col-md-6 noPadding">
-                            <form>
-                              <div class="form-group">
+
+
+
+                    <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off">
+                        <div class="row">
+                            <div class="col-md-6 noPadding">
+                                <div class="form-group">
                                     <label for="exampleInputPrename">Vorname</label>
-                                    <input type="text" name="vorname" class="form-control form" placeholder="%Vorname%" value="<?php if(isset($vorname)) { echo $vorname; }else { echo $myUser->getsFirstName();  }?>">
+                                    <input type="text" name="vorname" class="form-control form" value="<?php if(isset($vorname))  echo $vorname; ?>"  placeholder="<?php echo $myUser->getsFirstName(); ?>" >
                                     <span class="text-danger"><?php if(isset($vornameError)) echo $vornameError; ?></span>
-                              </div>
-                              <div class="form-group">
+                                </div>
+                                <div class="form-group">
                                     <label for="exampleInputLastname">Nachname</label>
-                                    <input type="text" name="nachname" class="form-control form" placeholder="%Nachname%" value="<?php if(isset($nachname)) echo $nachname;  else echo $myUser->getsLastName(); ?>">
+                                    <input type="text" name="nachname" class="form-control form" placeholder="<?php if(isset($nachname)) echo $nachname;  else echo $myUser->getsLastName(); ?>">
                                     <span class="text-danger"><?php if(isset($nachnameError)) echo $nachnameError; ?></span>
-                              </div> 
-                            </form>    
-                        </div>
-                        <div class="col-md-6 noPadding">
-                            <form>
-                              <div class="form-group">
+                                </div>
+
+                            </div>
+                            <div class="col-md-6 noPadding">
+
+                                <div class="form-group">
                                     <label for="exampleInputUsername">Benutzername</label>
-                                    <input type="text" name="username" class="form-control form" placeholder="%UserName%" value="<?php if(isset($username)){ echo $username;} else {echo $myUser->getsUsername();} ?>">
+                                    <input type="text" name="username" class="form-control form" placeholder="<?php if(isset($username)){ echo $username;} else {echo $myUser->getsUsername();} ?>">
                                     <span class="text-danger"><?php if(isset($usernameError)) echo $usernameError; ?></span>
-                              </div>
-                              <div class="form-group">
+                                </div>
+                                <div class="form-group">
                                     <label for="exampleInputMail"><span class="glyphicon glyphicon-envelope"></span> E-Mail Adresse</label>
-                                    <input type="email"  name="email" class="form-control form" placeholder="%EMail%"  value="<?php if(isset($email)){ echo $email; } else {$myUser->getsEMail(); }?>">
+                                    <input type="email" name="email" class="form-control form" placeholder="<?php if(isset($email)){ echo $email; } else {echo $myUser->getsEmail(); }?>">
                                     <span class="text-danger"><?php if(isset($emailError)) echo $emailError; ?></span>
-                              </div>
-                            </form>    
+                                </div>
+
+                            </div>
                         </div>
-                    </div>
-                    
-                    <div id="" class="row">
-                        <div class="col-md-2 noPadding">
-                                <a  href="../HTML/userOverview.html"><button id="PinkButton" class="btn btn-default" href="userOverview.html">Speichern</button></a>
+
+                        <div id="" class="row">
+                            <div class="col-md-2 noPadding">
+                                <button id="PinkButton" type="submit" class="btn btn-block btn-primary" name="btn-signup">Speichern</button>
                             </div>
                             <div class="col-md-2 noPadding">
-                                <a  href="../HTML/userOverview.html"><button id="Button" type="submit" name="btn-save" class="btn btn-default">Abbrechen</button></a>
+                                <a href="../HTML/userOverview.html"><button id="Button" type="submit" name="btn-save" class="btn btn-default">Abbrechen</button></a>
                             </div>
                         </div>
-                    </div>
-                    
-                </div> 
-    
+                    </form>
+
+
+
+
+                </div>
+
             </div>
-            
-        </div>    
 
     </body>
 
-</html>
+    </html>

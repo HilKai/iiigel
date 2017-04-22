481f2eb716804db5237f5e393f56d67b42b33805
CorinnaHe <hei.cor@web.de>
2017-04-22 17:13:27 +0200

    Added Delete User

--- PHP/AdminUserView.php
+++ PHP/AdminUserView.php
@@ -3,6 +3,7 @@
 	 session_start();
 	 $myPage = file_get_contents('../HTML/AdminUserView.html');
 	 include_once("database.php");
+     $allUsers = $ODB->getAllUsers();
 
     // if session is not set this will redirect to login page
 	 if( !isset($_SESSION['user']) ) {
@@ -10,15 +11,27 @@
 	  exit;
 	 }
 
-    $toAdd = "";
+    if ($_POST){
 
-    $allUsers = $ODB->getAllUsers();
+        if(isset($_POST['DeleteUser'])){      
+                 for ($i=0; $i< sizeof($allUsers);$i++){  
+                    if($allUsers[$i]->getID() ==  $_POST['DeleteUser']) {
+                        echo "pipsi";
+                        $ODB->deleteUser($allUsers[$i]->getID());
+                        header("Refresh:0");     
+                    }
+                }
+        }
+    }
+
+    $toAdd = "";
 
     for ($i=0; $i< sizeof($allUsers);$i++){   
         $myRow = file_get_contents('../HTML/AdminUserViewTablerow.html');
-            $search = array('%Username%', '%Prename%', '%Lastname%', '%Mail%');
-            $replace = array($allUsers[$i]->getsUsername(), $allUsers[$i]->getsFirstName(), $allUsers[$i]->getsLastName(), $allUsers[$i]->getsEMail());
+            $search = array('%Username%', '%Prename%', '%Lastname%', '%Mail%', '%ID%');
+            $replace = array($allUsers[$i]->getsUsername(), $allUsers[$i]->getsFirstName(), $allUsers[$i]->getsLastName(), $allUsers[$i]->getsEMail(), $allUsers[$i]->getID() );
             $myRow = str_replace($search,$replace,$myRow);
+         
         
         $toAdd = $toAdd . $myRow;
        

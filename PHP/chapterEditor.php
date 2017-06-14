7129589d2a20b9c817a6a5beaf666c24b7b2d7eb
HilKai <Kai.Hilgers.Info@gmx.de>
2017-06-14 21:02:48 +0200

    Chapter Editor !TODO Berechtigungen, Funktion auskommentieren

---
+++ PHP/chapterEditor.php
@@ -0,0 +1,37 @@
+<?php
+	 ob_start();
+	 session_start();
+	 $myPage = file_get_contents('../HTML/chapterEditor.html');
+	 include_once("database.php");
+    
+
+    $myModuleID = $_GET['moduleID'];
+    $myChapterID = $_GET['chapterID'];
+    $myUserID = $_SESSION['user'];
+    
+	 // if session is not set this will redirect to login page
+    if( !isset($_SESSION['user']) ) {
+        header("Location: ../index.php");
+        exit;
+	 }
+
+    //TODO-Nur user mit Berechtigungen erlaubt
+
+
+  
+    
+    //
+
+    $myModule = $ODB->getModuleFromID($myModuleID);
+    $myUser = $ODB->getUserFromId($myUserID);
+    $search = array('%ChapterHeadline%','%ChapterText%');
+    $chapterText = $myModule->getChapterTextbyIndex($myChapterID);
+    $myPage = str_replace("%ChapterTextRaw%",$chapterText,$myPage);
+    $chapterText = $ODB->replaceTags($chapterText);
+    $text = '<div class="chapterView col-md-12">  '.$chapterText.'"</div>';
+    $replace = array($myModule->getChapterHeadlineByIndex($myChapterID),$text);
+    $myPage = str_replace($search,$replace,$myPage);
+   
+    
+echo $myPage;
+?>
---
+++ PHP/updateChapter.php
@@ -0,0 +1,5 @@
+<?php
+    inlcude_once("database.php");
+ //   $ODB->updateChapter($_GET['modulID'],$_GET['chapterID'],$_GET['text']);
+    
+?>

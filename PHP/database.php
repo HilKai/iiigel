6377473dab845175cd2a98cdd3ce34ae431b7317
Toby2403 <42702195+Toby2403@users.noreply.github.com>
2018-12-16 22:08:31 +0100

    Initial commit

--- PHP/database.php
+++ PHP/database.php
@@ -1265,7 +1265,6 @@
         }
         
         public function getFortschritt($UserID,$GroupID){
-
             if($_SESSION['lastDeletedUser']!=$UserID){
             $this->stmtGetFortschritt->bind_param("ii",$UserID,$GroupID);
             $this->stmtGetFortschritt->execute();
@@ -1276,7 +1275,8 @@
             } else {
               throw new exception('User ist nicht in dieser Gruppe.');
             }
-        }}
+           }
+        }
         
         public function getModuleFromGroup($GroupID){
             $this->stmtGetModuleFromGroup->bind_param("i",$GroupID);
